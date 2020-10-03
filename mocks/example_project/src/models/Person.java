23771d7f4d0b6ab1912d94b7ad4efa4553369bfb
Dennis Kats <denniskats43@gmail.com>
2020-10-03 15:55:12 -0400

    Updated Person example class

--- mocks/example_project/src/models/Person.java
+++ mocks/example_project/src/models/Person.java
@@ -1,14 +1,13 @@
 package mocks.example_project.src.models;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class Person {
     public int age;
     public String firstName;
     public String lastName;
     private final String socialSecurity;
-    public ArrayList<Person> friends;
+    private ArrayList<Person> friends;
 
     public Person(String firstName, String lastName, int age, String socialSecurity) {
         this.firstName = firstName;
@@ -26,30 +25,6 @@ public class Person {
         this.friends = friends;
     }
 
-    public int getAge() {
-        return this.age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getFirstName() {
-        return this.firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return this.lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getSocialSecurity() {
         return this.socialSecurity;
     }
@@ -60,30 +35,6 @@ public class Person {
 
     public void addFriend(Person friend) {
         this.friends.add(friend);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof Person)) {
-            return false;
-        }
-        Person person = (Person) o;
-        return age == person.age && Objects.equals(firstName, person.firstName)
-                && Objects.equals(lastName, person.lastName) && Objects.equals(socialSecurity, person.socialSecurity)
-                && Objects.equals(friends, person.friends);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(age, firstName, lastName, socialSecurity, friends);
-    }
-
-    @Override
-    public String toString() {
-        return "{" + " age='" + getAge() + "'" + ", firstName='" + getFirstName() + "'" + ", lastName='" + getLastName()
-                + "'" + ", socialSecurity='" + getSocialSecurity() + "'" + ", friends='" + getFriends() + "'" + "}";
     }
 
 }
